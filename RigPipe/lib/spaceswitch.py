4a0b0f98d9cc16cafd3aab71f6cb38438b941196
Yanis <yanis.levostre@gmail.com>
2023-12-12 20:57:35 -0500

    add some fixes naming add possibility to do multiple guides using a dictionary, delete transform input

--- RigPipe/lib/spaceswitch.py
+++ RigPipe/lib/spaceswitch.py
@@ -1,42 +1,56 @@
 import maya.cmds as cmds
 from constraint import multMatrix
-def do_spaceswitch(module_name= None,guide = None,transform = None,parent_list = None):
-    # create groups and add locators
 
-    spaceOffsets = cmds.group(em=True, n=module_name + '_spaceOffsets_grp')
-    spaceInputs = cmds.group(em=True, n=module_name + '_spaceInputs_grp')
-    cmds.parent(spaceOffsets, spaceInputs, module_name + '_input_grp')
-    blendMatrix = cmds.createNode('blendMatrix', n=module_name + '_blendMatrix')
+
+def do_spaceswitch(module_name=None, guideList=None, parent_list=None,
+                   channel=['translate', 'rotate', 'scale']):
+
+
+    # create groups and add locators
+    space_offsets = cmds.group(em=True, n=module_name + '_spaceOffsets_grp')
+    space_inputs = cmds.group(em=True, n=module_name + '_spaceInputs_grp')
+    cmds.parent(space_offsets, space_inputs, module_name + '_input_grp')
     follow = cmds.createNode('transform', n=module_name + "_followPlug_input")
-    outNode = transform
+
     drivers = parent_list
     enum_list = ":".join(drivers)
     cmds.addAttr(follow, ln='follow', k=True, at='enum', en=enum_list)
+    for i, driver in enumerate(drivers):
+        grp_offset = cmds.group(em=True, n='{}_{}_spaceOffset_grp'.format(module_name, driver))
+        grp_input = cmds.group(em=True, n='{}_{}_spaceInput_grp'.format(module_name, driver))
+        cmds.parent(grp_offset, space_offsets)
+        cmds.parent(grp_input, space_inputs)
+    for guide in guideList:
+        print (guide)
+        print(guideList)
+        name = guide.strip('_guide')
+        blend_matrix = cmds.createNode('blendMatrix', n=name + '_blendMatrix')
+        out_node = guideList[guide]
+        for i, driver in enumerate(drivers):
+            grp_offset = '{}_{}_spaceOffset_grp'.format(module_name, driver)
+            grp_input = '{}_{}_spaceInput_grp'.format(module_name, driver)
+            loc_space = cmds.spaceLocator(n='{}_{}_loc'.format(name, driver))[0]
+            cmds.parent(loc_space, grp_input)
 
-    for i,driver in enumerate(drivers):
-        grpOffset = cmds.group(em = True, n = '{}_{}_spaceOffset_grp'.format(module_name,driver))
-        grpInput = cmds.group(em=True, n='{}_{}_spaceInput_grp'.format(module_name, driver))
-        locSpace = cmds.spaceLocator(n = '{}_{}_loc,'.format(module_name, driver))[0]
-        cmds.parent(locSpace,grpInput)
-        cmds.parent(grpOffset,spaceOffsets)
-        cmds.parent(grpInput, spaceInputs)
+            # connect guide to space input
+            multMatrix(source=guide, target=loc_space, offset=grp_offset)
 
-        # connect guide to space input
-        multMatrix(source = guide, target = locSpace,offset = grpOffset)
+            # create choices and blend
+            cmds.connectAttr(loc_space + '.worldMatrix[0]', blend_matrix + '.target[' + str(i) + '].targetMatrix')
+            cond = cmds.createNode('condition', n=name + driver + '_condition')
+            cmds.setAttr(cond + '.secondTerm', i)
+            cmds.setAttr(cond + '.colorIfTrueR', 1)
+            cmds.setAttr(cond + '.colorIfFalseR', 0)
+            cmds.connectAttr(follow + '.follow', cond + '.firstTerm')
+            cmds.connectAttr(cond + '.outColorR', blend_matrix + '.target[' + str(i) + '].useMatrix')
 
-        # create choices and blend
-        cmds.connectAttr( locSpace+ '.worldMatrix[0]', blendMatrix + '.target[' + str(i) + '].targetMatrix')
-        cond = cmds.createNode('condition', n=module_name + driver + '_condition')
-        cmds.setAttr(cond + '.secondTerm', i)
-        cmds.setAttr(cond + '.colorIfTrueR', 1)
-        cmds.setAttr(cond + '.colorIfFalseR', 0)
-        cmds.connectAttr(follow + '.follow', cond + '.firstTerm')
-        cmds.connectAttr(cond + '.outColorR', blendMatrix + '.target[' + str(i) + '].useMatrix')
+        # connect to the out transform
+        dec = cmds.createNode('decomposeMatrix', n=name + 'follow_decomposeMatrix')
+        cmds.connectAttr(blend_matrix + '.outputMatrix', dec + '.inputMatrix')
 
-    # connect to the out transform
-    dec = cmds.createNode('decomposeMatrix', n=module_name + 'follow_decomposeMatrix')
-    cmds.connectAttr(blendMatrix + '.outputMatrix', dec + '.inputMatrix')
-
-    cmds.connectAttr(dec + '.outputTranslate', outNode + '.translate', f = True)
-    cmds.connectAttr(dec + '.outputRotate', outNode + '.rotate', f = True)
-    cmds.connectAttr(dec + '.outputScale', outNode + '.scale', f = True)
+        if 'translate' in channel:
+            cmds.connectAttr(dec + '.outputTranslate', out_node + '.translate', f=True)
+        if 'rotate' in channel:
+            cmds.connectAttr(dec + '.outputRotate', out_node + '.rotate', f=True)
+        if 'scale' in channel:
+            cmds.connectAttr(dec + '.outputScale', out_node + '.scale', f=True)
